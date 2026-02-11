3ed136086b1c24183e03ae304ddfa0546b30813b
Manuel Fuentes <mfuecru1804@g.educaand.es>
2026-02-11 20:42:34 +0100

    Update main

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,34 @@
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Main {
-
-
-    public static final String X = "================================================================";
-
-
     public static void main(String[] args){
-        String dollar = "\u0024";
         Scanner sc = new Scanner(System.in);
         ArrayList<Juego> juegos = new ArrayList<>();
-        int opcion;
+
+        System.out.println("Elige la opcion\n1.Ingresar Juego\n2.Vender Juego\n3.Ver Juegos");
+        int opcion = sc.nextInt();
 
 
-
-
-
-
-        do {
-            System.out.println(X);
-            System.out.println(X);
-            System.out.println("Elige la opcion\n1.Ingresar Juego\n2.Vender Juego\n3.Ver Juegos\n4.Limpiar Pantalla");
-            System.out.println(X);
-            opcion = sc.nextInt();
-            sc.nextLine();
-
-
-
-
+        while(opcion!=0){
             switch (opcion){
                 case 1:
 
-                    Juego juego = new Juego();
-                    juego.RegistrarJuegos();
-                    juegos.add(juego);
-                    System.out.println("Juego creado :" + juego.getTitulo());
+                    Juego juego1 = new Juego();
+                    juego1.RegistrarJuegos();
+                    System.out.println("Juego creado :" + juego1.getTitulo());
+
                     break;
 
                 case 2:
                     break;
 
-
                 case 3:
-                    for (int i = 0; i < juegos.size(); i++) {
-                        System.out.println("||Titulo : " + juegos.get(i).getTitulo()+ "||Precio : " + juegos.get(i).getPrecio() + dollar + "||Genero : " + juegos.get(i).getGenero());
-                    }
-                    System.out.println("Esperando un enter");
-                    sc.nextLine();
-                    break;
-
-                case 4:
-
-                    try {
-                        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-                    } catch (IOException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
                     break;
             }
-        }while (opcion != 0);
 
+
+        }
 
     }
 }
-
