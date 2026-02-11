70b73c889bda85a02223a8005c408c544f872f67
Manuel Fuentes <mfuecru1804@g.educaand.es>
2026-02-11 17:24:04 +0100

    Update de Juego.java e incorporacion de menu

--- src/Juego.java
+++ src/Juego.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Juego {
+    Scanner sc = new Scanner(System.in);
     String titulo;
-    double precio;
+    double precio ;
     String genero;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void RegistrarJuegos() {
+        System.out.println("Ingrese el nombre del juego con _");
+        titulo = sc.nextLine();
+        System.out.println("Ingrese el precio del juego");
+        precio = Double.parseDouble(sc.nextLine());
+        System.out.println("Ingrese el genero del juego");
+        genero = sc.nextLine();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Juego> juegos = new ArrayList<>();
+
+        System.out.println("Elige la opcion\n1.Ingresar Juego\n2.Vender Juego\n3.Ver Juegos");
+        int opcion = sc.nextInt();
+
+
+        while(opcion!=0){
+            switch (opcion){
+                case 1:
+
+                    Juego juego1 = new Juego();
+                    juego1.RegistrarJuegos();
+                    System.out.println("Juego creado :" + juego1.getTitulo());
+
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+            }
+
+
+        }
+
+
+
 
     }
 }
